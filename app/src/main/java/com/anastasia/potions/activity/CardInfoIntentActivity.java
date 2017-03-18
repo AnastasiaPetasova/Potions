a2e1c31a17c532ad072152271e789b51ca559b0b
Слава Муравьёв <MuravjevSlava@gmail.com>
2017-03-18 17:49:28 +0400

    Added interface for activity, which works with card info intents

---
+++ app/src/main/java/com/anastasia/potions/activity/CardInfoIntentActivity.java
@@ -0,0 +1,9 @@
+package com.anastasia.potions.activity;
+
+public interface CardInfoIntentActivity {
+
+    String OWNER = "owner";
+
+    String INGREDIENT = "ingredient";
+    String RECIPE = "recipe";
+}
