b0d569fe4956841625b9cc3215d821acde82efb6
Слава Муравьёв <MuravjevSlava@gmail.com>
2017-03-19 21:15:13 +0400

    Added localization for recipes Added 'prism' recipe that is mixed from sun and ice

--- app/src/main/java/com/anastasia/potions/card/Recipe.java
+++ app/src/main/java/com/anastasia/potions/card/Recipe.java
@@ -6,8 +6,9 @@ import java.util.List;
 
 public enum Recipe implements Iterable<Recipe> {
 
-    WATER(), FIRE(),
-    ICE(2, WATER, WATER), SUN(2, FIRE, FIRE), TOOTH();
+    WATER(), FIRE(), TOOTH(),
+    ICE(2, WATER, WATER), SUN(2, FIRE, FIRE),
+    PRISM(4, ICE, SUN);
 
     public final int score;
     public final List<Recipe> mixedRecipes;
@@ -21,9 +22,8 @@ public enum Recipe implements Iterable<Recipe> {
         this.mixedRecipes = Arrays.asList(recipes);
     }
 
-    // TODO change after full 'russianization'
     public String getLocaleName() {
-        return name();
+        return RecipeLocalization.getLocalName(this);
     }
 
     public boolean isIngredient() {
