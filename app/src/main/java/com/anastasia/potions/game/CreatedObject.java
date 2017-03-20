4a03a554f3eb627e6a0bbacf2e7fd95a72574099
Слава Муравьёв <MuravjevSlava@gmail.com>
2017-03-20 08:20:46 +0400

    Updated CreatedObject: added getter for serializable list of used cards

--- app/src/main/java/com/anastasia/potions/game/CreatedObject.java
+++ app/src/main/java/com/anastasia/potions/game/CreatedObject.java
@@ -3,6 +3,8 @@ package com.anastasia.potions.game;
 import com.anastasia.potions.card.Card;
 import com.anastasia.potions.card.Recipe;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CreatedObject {
@@ -12,12 +14,16 @@ public class CreatedObject {
     public final Recipe baseRecipe;
     public final Card baseCard;
 
-    List<Card> usedCards;
+    private List<Card> usedCards;
 
-    public CreatedObject(Player player, Recipe baseRecipe, Card baseCard, List<Card> usedCards) {
+    CreatedObject(Player player, Recipe baseRecipe, Card baseCard, List<Card> usedCards) {
         this.player = player;
         this.baseRecipe = baseRecipe;
         this.baseCard = baseCard;
         this.usedCards = usedCards;
+    }
+
+    public Serializable getSerializableUsedCards() {
+        return (ArrayList<Card>) usedCards;
     }
 }
