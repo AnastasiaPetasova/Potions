f8cb8524432803e08803919c8384efe12a89b738
Слава Муравьёв <MuravjevSlava@gmail.com>
2017-03-21 10:34:24 +0400

    Added interface with taking object results for recipe creating

--- app/src/main/java/com/anastasia/potions/game/creating/RecipeCreatingConstants.java
+++ app/src/main/java/com/anastasia/potions/game/creating/RecipeCreatingConstants.java
@@ -1,6 +1,6 @@
 package com.anastasia.potions.game.creating;
 
-public interface RecipeBuildConstants {
+public interface RecipeCreatingConstants {
 
     int NOT_NEEDED = 0, ALREADY_FULL = NOT_NEEDED + 1, TAKEN = ALREADY_FULL + 1;
 }
