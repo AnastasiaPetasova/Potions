5c5c0fca6ffab63401873f037c272074ae7630b9
Слава Муравьёв <MuravjevSlava@gmail.com>
2017-03-21 10:20:43 +0400

    CardAdapter and CreatedObjectAdapter set correct background for created views now

--- app/src/main/java/com/anastasia/potions/adapter/CardAdapter.java
+++ app/src/main/java/com/anastasia/potions/adapter/CardAdapter.java
@@ -2,6 +2,7 @@ package com.anastasia.potions.adapter;
 
 import android.content.Context;
 
+import com.anastasia.potions.R;
 import com.anastasia.potions.card.Card;
 import com.anastasia.potions.view.CardView;
 
@@ -14,6 +15,8 @@ public class CardAdapter extends GameListAdapter<Card, CardView> {
     @Override
     protected CardView createView(Card card) {
         CardView cardView = new CardView(context);
+        cardView.setBackgroundResource(R.drawable.background_selector_card);
+
         cardView.setCard(card);
 
         return cardView;
--- app/src/main/java/com/anastasia/potions/adapter/CreatedObjectAdapter.java
+++ app/src/main/java/com/anastasia/potions/adapter/CreatedObjectAdapter.java
@@ -2,7 +2,8 @@ package com.anastasia.potions.adapter;
 
 import android.content.Context;
 
-import com.anastasia.potions.game.creating.CreatedObject;
+import com.anastasia.potions.R;
+import com.anastasia.potions.game.created.CreatedObject;
 import com.anastasia.potions.view.CreatedObjectView;
 
 public class CreatedObjectAdapter extends GameListAdapter<CreatedObject, CreatedObjectView> {
@@ -14,6 +15,8 @@ public class CreatedObjectAdapter extends GameListAdapter<CreatedObject, Created
     @Override
     protected CreatedObjectView createView(CreatedObject createdObject) {
         CreatedObjectView createdObjectView = new CreatedObjectView(context);
+        createdObjectView.setBackgroundResource(R.drawable.background_selector_object);
+
         createdObjectView.setCreatedObject(createdObject);
 
         return createdObjectView;
