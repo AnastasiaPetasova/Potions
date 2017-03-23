28f3fb3b98171246004b7b661ad9c7eab7f2c743
Слава Муравьёв <MuravjevSlava@gmail.com>
2017-03-23 06:22:08 +0400

    Added util class 'Pair'

---
+++ app/src/main/java/com/anastasia/potions/util/Pair.java
@@ -0,0 +1,11 @@
+package com.anastasia.potions.util;
+
+public class Pair {
+
+    public int first, second;
+
+    public Pair(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+}
